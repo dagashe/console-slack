ec31975a18ff9e7b29b8a396f97b65bef95a24e3
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-03-12 20:33:43 +1000

    add an onsuccess handler, and refactor the error checking.

--- index.js
+++ index.js
@@ -5,30 +5,58 @@ var exports = {
     webhook : false
 };
 
-exports.checkSettings = function(){
+exports.checkSettings = function(channel, onSuccess){
     // check to make sure the setting are correct
 
     // If the webhook is not set, we can't post to slack.
-    if (!webhook){
+    if (!exports.webhook){
         throw new Error("Webhook URL not provided.  Add it with `slack.webhook = <url>`");
     }
-    // this is pretty much the only required setting.
+
+    // make sure that the onSuccess function is actually a function
+    if (onSuccess && typeof onSuccess !== 'function'){
+        throw new TypeError('onSuccess must be a function');
+    }
+
+    // make sure the channel begins with a #
+    if (!/^#.*/.test(channel)){
+        throw new SyntaxError("Channels must start with a #.  Yours was " + channel + ".");
+    }
 
 };
 
-console.slack = function(message, channel){
+console.slack = function(message, channel, onSuccess){
     if (exports.options){ // this is unfortunate, but we only have to do it once...
         exports.channel  = exports.options.channel  || exports.channel;
         exports.username = exports.options.username || exports.username;
         exports.emoji    = exports.options.emoji    || exports.emoji;
         exports.webhook  = exports.options.webhook  || exports.webhook;
     }
-    exports.checkSettings();
+
     channel = channel || exports.channel;
-    if (webhook == 'test'){
-        console.log(exports.username + " says " + message + " to " + channel);
+
+    exports.checkSettings(channel, onSuccess);
+
+    if (exports.webhook == 'test'){
+        return console.log(exports.username + " says " + message + " to " + channel);
     }
-    console.log("Slack: " + message);
+
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if (request.readyState == 4){ // the request is complete
+            onSuccess(request.response, request.status);
+        }
+    };
+
+    request.open('POST', exports.webhook, true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send({
+        text : message,
+        username : exports.username,
+        icon_emoji : exports.emoji,
+        channel : channel
+    });
+
 
 };
 
