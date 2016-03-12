2258aa2b0d5304cad65fd4a04d86d49e8b6a7ad5
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-03-12 18:35:27 +1000

    process any options we may be passed.

--- console-slack.js
+++ console-slack.js
@@ -12,11 +12,17 @@ exports.checkSettings = function(){
     if (!webhook){
         throw new Error("Webhook URL not provided.  Add it with `slack.webhook = <url>`");
     }
-
+    // this is pretty much the only required setting.
 
 };
 
 console.slack = function(message, channel){
+    if (exports.options){ // this is unfortunate, but we only have to do it once...
+        exports.channel  = exports.options.channel  || exports.channel;
+        exports.username = exports.options.username || exports.username;
+        exports.emoji    = exports.options.emoji    || exports.emoji;
+        exports.webhook  = exports.options.webhook  || exports.webhook;
+    }
     exports.checkSettings();
     channel = channel || exports.channel;
     if (webhook == 'test'){
